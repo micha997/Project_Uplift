38733efffa49c0c43ca899b34f567a53b3fd66fa
Tristan <me@tristanhsr.de>
2017-05-07 16:42:01 +0200

    fuege aufgabe2 hinzu

--- kickoff_aufgabe/kickoff_aufgabe.js
+++ kickoff_aufgabe/kickoff_aufgabe.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
+var chalk = require('chalk');
 
 fs.readFile(__dirname+"/staedte.json", function(err,data){
 	var staedte = JSON.parse(data);
 	
 	for(var i = 0;i < staedte.cities.length;i++){
-		console.log("name: "+staedte.cities[i].name);
-		console.log("country: "+staedte.cities[i].country);
-		console.log("population: "+staedte.cities[i].population);
+		console.log("name: "+chalk.blue(staedte.cities[i].name));
+		console.log("country: "+chalk.green(staedte.cities[i].country));
+		console.log("population: "+chalk.yellow(staedte.cities[i].population));
 		console.log("\n--------------------\n");	
 	}
 });
