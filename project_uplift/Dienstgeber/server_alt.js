9e0b5ef8f654b0bb0365110d3acf112c5c0af1ab
micha997 <mail@michael-michel.com>
2017-06-26 11:46:50 +0200

    merge server bugfix

---
+++ project_uplift/Dienstgeber/server_alt.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+const settings = {
+    port: 3773
+};
+
+
+app.use(bodyParser.json());
+//Pfad fuer Favroute Funktionen
+app.use('/favroute', require('./ressourcen/favroute'));
+
+//Weitere Funktioenen
+
+
+//Server starten
+app.listen(setting.port, function () {
+   console.log("REST-Sever läuft auf Port " + settings.port);
+});
