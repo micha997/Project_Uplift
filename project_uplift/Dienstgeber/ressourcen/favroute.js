1c820bd3558a3d6d5d5c2fc838ec257b819f359d
micha997 <mail@michael-michel.com>
2017-06-26 13:38:32 +0200

    json parse funktioniert in favroute

--- project_uplift/Dienstgeber/ressourcen/favroute.js
+++ project_uplift/Dienstgeber/ressourcen/favroute.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
+const express = require("express");
+const router = express.Router();
+const bodyParser = require('body-parser')
 
+const ressourceName = "favroute";
 
 //Middleware
 router.use(function timelog (req, res, next){
@@ -9,19 +10,21 @@ router.use(function timelog (req, res, next){
 	next();
 });
 
-router.post('/', function(req,res){
-   
-    var contentTyp = req.get('Content-Type');
-    
-    if(contentTyp != "application/json"){
+router.post('/', bodyParser.json(), function(req,res){
+   //Content-Type des Headers pruefen
+   var contentType = req.get('Content-Type');
+   if(contentType != "application/json"){
         res.set("Accepts", "application/json").status(406).end();
-    }else{
+   }else{
         var newFavRoute = req.body;
         //Weiter bearbeitung der json
         
-        
         res.set("Content-Type", 'application/json').set("Location", "/favroute/").status(201).json(newFavRoute).end();
-    }
+   }
+});
+
+router.get('/', function(req, res){
+	res.send("GET Hello World!");
 });
 
 module.exports = router;
--- project_uplift/Dienstgeber/server.js
+++ project_uplift/Dienstgeber/server.js
@@ -1,41 +1,41 @@
 //module 
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
 
 var app = express();
+//var bodyParser = require('body-parser');
 
 //Settings festlegen
 const settings = {
 	port: 3773
 };
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 //faveroute ueber Pfad holen
-app.use("/favroute",require('./ressourcen/favroute'));
-
+const favroute = require('./ressourcen/favroute');
+app.use("/favroute", favroute);
 //
 //Versucht es so ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //
 
-//bewertung über Pfad holen
+//bewertung ueber Pfad holen
 const bewertung = require('./bewertung');
 app.use("/bewertung",bewertung);
 
-//equipment über Pfad holen
+//equipment ueber Pfad holen
 const equipment = require('./equipment');
 app.use("/equipment",equipment);
 
-//faveroute über Pfad holen
-const favroute = require('./favroute');
-app.use("/favroute",favroute);
+//faveroute ueber Pfad holen
+//const favroute = require('./favroute');
+//app.use("/favroute",favroute);
 
-//route über Pfad holen
+//route ueber Pfad holen
 const route = require('./route');
 app.use("/route",route);
 
-//routetoequipment über Pfad holen
+//routetoequipment ueber Pfad holen
 const routetoequipment = require('./routetoequipment');
 app.use("/routetoequipment",routetoequipment);
 
